fa4608be589eedf55f8a755d961ddb493d069c1e
FrowntownPitt <ajf93@pitt.edu>
2018-10-31 16:07:02 -0400

    Update schema to have lower snake_case

--- gibgab_db.sql
+++ gibgab_db.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema gibgab
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema gibgab
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `gibgab` DEFAULT CHARACTER SET utf8 ;
+USE `gibgab` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Campus`
+-- Table `gibgab`.`campus`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Campus` (
+CREATE TABLE IF NOT EXISTS `gibgab`.`campus` (
   `pk_campus_id` INT NOT NULL AUTO_INCREMENT,
   `campus_name` VARCHAR(45) NULL,
   `latitude` DOUBLE NULL,
@@ -27,29 +27,29 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`User`
+-- Table `gibgab`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`User` (
+CREATE TABLE IF NOT EXISTS `gibgab`.`user` (
   `pk_user_id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(45) NULL,
   `username` VARCHAR(45) NULL,
-  `is_moderator` TINYINT NULL,
-  `password_hash` VARCHAR(45) NULL,
+  `user_status` TINYINT NULL,
+  `password_hash` BINARY(60) NULL,
   `fk_campus_id` INT NOT NULL,
   PRIMARY KEY (`pk_user_id`),
   INDEX `fk_User_Campus1_idx` (`fk_campus_id` ASC),
   CONSTRAINT `fk_User_Campus1`
     FOREIGN KEY (`fk_campus_id`)
-    REFERENCES `mydb`.`Campus` (`pk_campus_id`)
+    REFERENCES `gibgab`.`campus` (`pk_campus_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Post`
+-- Table `gibgab`.`post`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Post` (
+CREATE TABLE IF NOT EXISTS `gibgab`.`post` (
   `pk_post_id` INT NOT NULL AUTO_INCREMENT,
   `text` VARCHAR(300) NULL,
   `image` BLOB NULL,
@@ -63,21 +63,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Post` (
   INDEX `fk_Content_Campus1_idx` (`fk_campus_id` ASC),
   CONSTRAINT `fk_Content_User10`
     FOREIGN KEY (`fk_author_id`)
-    REFERENCES `mydb`.`User` (`pk_user_id`)
+    REFERENCES `gibgab`.`user` (`pk_user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Content_Campus10`
     FOREIGN KEY (`fk_campus_id`)
-    REFERENCES `mydb`.`Campus` (`pk_campus_id`)
+    REFERENCES `gibgab`.`campus` (`pk_campus_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Comment`
+-- Table `gibgab`.`comment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Comment` (
+CREATE TABLE IF NOT EXISTS `gibgab`.`comment` (
   `pk_comment_id` INT NOT NULL AUTO_INCREMENT,
   `text` VARCHAR(300) NULL,
   `author_name` VARCHAR(45) NULL,
@@ -90,21 +90,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Comment` (
   INDEX `fk_Comment_Post1_idx` (`fk_post_id` ASC),
   CONSTRAINT `fk_Content_User1`
     FOREIGN KEY (`fk_author_id`)
-    REFERENCES `mydb`.`User` (`pk_user_id`)
+    REFERENCES `gibgab`.`user` (`pk_user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Comment_Post1`
     FOREIGN KEY (`fk_post_id`)
-    REFERENCES `mydb`.`Post` (`pk_post_id`)
+    REFERENCES `gibgab`.`post` (`pk_post_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`CommentVote`
+-- Table `gibgab`.`comment_vote`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`CommentVote` (
+CREATE TABLE IF NOT EXISTS `gibgab`.`comment_vote` (
   `pk_comment_vote_id` INT NOT NULL AUTO_INCREMENT,
   `upvote` TINYINT NULL,
   `fk_author_id` INT NOT NULL,
@@ -114,21 +114,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`CommentVote` (
   INDEX `fk_Vote_Content1_idx` (`fk_content_id` ASC),
   CONSTRAINT `fk_Vote_User`
     FOREIGN KEY (`fk_author_id`)
-    REFERENCES `mydb`.`User` (`pk_user_id`)
+    REFERENCES `gibgab`.`user` (`pk_user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Vote_Content1`
     FOREIGN KEY (`fk_content_id`)
-    REFERENCES `mydb`.`Comment` (`pk_comment_id`)
+    REFERENCES `gibgab`.`comment` (`pk_comment_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`BanEvent`
+-- Table `gibgab`.`ban_event`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`BanEvent` (
+CREATE TABLE IF NOT EXISTS `gibgab`.`ban_event` (
   `pk_ban_event_id` INT NOT NULL AUTO_INCREMENT,
   `start_date` DATETIME NOT NULL,
   `end_date` DATETIME NOT NULL,
@@ -139,21 +139,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`BanEvent` (
   INDEX `fk_BanEvent_User2_idx` (`fk_banned_id` ASC),
   CONSTRAINT `fk_BanEvent_User1`
     FOREIGN KEY (`fk_banner_id`)
-    REFERENCES `mydb`.`User` (`pk_user_id`)
+    REFERENCES `gibgab`.`user` (`pk_user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_BanEvent_User2`
     FOREIGN KEY (`fk_banned_id`)
-    REFERENCES `mydb`.`User` (`pk_user_id`)
+    REFERENCES `gibgab`.`user` (`pk_user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`PostVote`
+-- Table `gibgab`.`post_vote`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`PostVote` (
+CREATE TABLE IF NOT EXISTS `gibgab`.`post_vote` (
   `pk_post_vote_id` INT NOT NULL AUTO_INCREMENT,
   `upvote` TINYINT NULL,
   `fk_author_id` INT NOT NULL,
@@ -163,21 +163,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`PostVote` (
   INDEX `fk_PostVote_Post1_idx` (`pk_post_id` ASC),
   CONSTRAINT `fk_Vote_User0`
     FOREIGN KEY (`fk_author_id`)
-    REFERENCES `mydb`.`User` (`pk_user_id`)
+    REFERENCES `gibgab`.`user` (`pk_user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_PostVote_Post1`
     FOREIGN KEY (`pk_post_id`)
-    REFERENCES `mydb`.`Post` (`pk_post_id`)
+    REFERENCES `gibgab`.`post` (`pk_post_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Username`
+-- Table `gibgab`.`username`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Username` (
+CREATE TABLE IF NOT EXISTS `gibgab`.`username` (
   `pk_username_id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NULL,
   `fk_user_id` INT NOT NULL,
@@ -186,7 +186,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Username` (
   INDEX `fk_Usernames_User1_idx` (`fk_user_id` ASC),
   CONSTRAINT `fk_Usernames_User1`
     FOREIGN KEY (`fk_user_id`)
-    REFERENCES `mydb`.`User` (`pk_user_id`)
+    REFERENCES `gibgab`.`user` (`pk_user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
